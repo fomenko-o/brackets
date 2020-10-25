0ef0cc10ce6f06901302cee600e48b2f3864c3e7
Oleksandra Fomenko <oleksandra.fomenko@gmail.com>
2020-10-25 21:37:18 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = [];
+    let openBrackets = [];
+    let closeBrackets = [];
+
+    bracketsConfig.forEach((item) => {
+        openBrackets.push(item[0]);
+        closeBrackets.push(item[1]);
+    });
+
+    for (let i = 0; i < str.length; i++) {
+        if (closeBrackets.includes(str[i]) && arr.length !== 0) {
+            let pairBracket = openBrackets[closeBrackets.indexOf(str[i])];
+            if (
+                arr[arr.length - 1] === pairBracket ||
+                arr[arr.length - 1] === str[i]
+            ) {
+                arr.pop();
+            } else if (str[i] !== pairBracket) {
+                return false;
+            } else {
+                arr.push(str[i]);
+            }
+        } else {
+            arr.push(str[i]);
+        }
+    }
+    return arr.length === 0;
+};
